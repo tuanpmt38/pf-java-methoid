cfd0dab14c895982e32c6b446397d5bac82f6553
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-23 16:53:14 +0700

    Methoid Pentagonal

---
+++ Pentagonal.java
@@ -0,0 +1,25 @@
+package methoid;
+
+import java.util.Scanner;
+
+public class Pentagonal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Input n:");
+            number = scanner.nextInt();
+            if (number < 1)
+                System.out.println("N must be greater than 1:");
+            int n = getPentagonalNumber(number);
+            System.out.println("The number Pentagonl:" + n);
+        } while (number >= 1);
+
+    }
+
+    public static int getPentagonalNumber(int n) {
+        return n = (n * ((3 * n) - 1)) / 2;
+
+    }
+}
