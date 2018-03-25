4ce94bba0018178fc263fcc7b285c4c04ac0583a
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-25 15:14:10 +0700

    Method Sum digits number

--- SumDigits.java
+++ SumDigits.java
@@ -8,7 +8,16 @@ public class SumDigits {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input digits n: ");
         n = scanner.nextInt();
+        System.out.println("Display digits n: " + totalDigitsOfNumber(n));
+    }
 
+    public static int totalDigitsOfNumber(int n) {
+        int total = 0;
+        do {
+            total = total + n % 10;
+            n = n / 10;
+        } while (n > 0);
+        return total;
     }
 
 }
