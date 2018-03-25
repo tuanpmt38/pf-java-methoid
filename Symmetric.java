7f9e6dab78b94dc345f7bf81bbc96404b688206b
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-25 14:19:32 +0700

    Methoid Symmetric number

--- Symmetric.java
+++ Symmetric.java
@@ -2,17 +2,17 @@ package methoid;
 
 import java.util.Scanner;
 
-public class Plindrome {
+public class Symmetric {
     public static void main(String[] args) {
         int number;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
         number = scanner.nextInt();
         System.out.println("Reverse number : ");
-        System.out.println(plindromeNumber(number));
+        System.out.println(symmetricNumbers(number));
     }
 
-    public static int plindromeNumber(int number) {
+    public static int symmetricNumbers(int number) {
         int reverse = 0;
         int surplus;
         while (number > 0) {
