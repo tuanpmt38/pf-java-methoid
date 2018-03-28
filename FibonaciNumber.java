5fba2959b0c644f34ef657f395c835a1256208fe
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-28 16:53:44 +0700

    Method fibonaci number

--- FibonaciNumber.java
+++ FibonaciNumber.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class FibonaciNumber {
     public static void main(String[] args) {
-        for(int i=0; i<10;i++){
-            System.out.println("Fibonaci(" + i + ") = " + findFibonaci(i));
-        }
-
+        System.out.println("Find the number in the fibonacci range");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an index in the fibonacci range:");
+        int index = scanner.nextInt();
+        System.out.println("Fibonaci(" + index + ") = " + findFibonaci(index));
     }
-    public  static  long findFibonaci (int n){
-        if(n<3){
+
+    public static long findFibonaci(int n) {
+        if (n < 3) {
             return 1;
         }
-        return findFibonaci(n-1)+ findFibonaci(n-2);
+        return findFibonaci(n - 1) + findFibonaci(n - 2);
     }
 }
