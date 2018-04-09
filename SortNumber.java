12e559031f117389830f77197aa29b5c5ddce461
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-10 00:22:47 +0700

    Method sort number

--- SortNumber.java
+++ SortNumber.java
@@ -2,24 +2,26 @@ package methoid;
 
 public class SortNumber {
     public static void main(String[] args) {
-        int [] number= {6,7,3};
+        int[] number = {6, 7, 3, 19, 89, 6};
         sortNumber(number);
-        System.out.println("The sequence number is gradually increased: " );
+        System.out.println("The sequence number is gradually increased: ");
         display(number);
     }
-    public static void display(int [] arr) {
+
+    public static void display(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
-    public static void  sortNumber (int a[]){
-        int temp ;
-        for(int i=0;i<a.length;i++){
-            for(int j=a.length-1;j>=i;j--){
-                if(a[i]>a[j]){
-                     temp = a[i];
-                    a[i]=a[j];
-                    a[j]=temp;
+
+    public static void sortNumber(int a[]) {
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = a.length - 1; j > 0; j--) {
+                if (a[j] < a[j - 1]) {
+                    temp = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = temp;
                 }
             }
         }
